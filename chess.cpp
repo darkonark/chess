7ee438de5ab7c16d73bedde028edc9ad71a037e3
darkonark <darkonark@yahoo.com>
2017-02-21 14:39:44 -0500

    Add files via upload

---
+++ chess.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+class piece {
+	public:
+	//stuff
+};
+class king {
+	public:
+//	void initiate (button interrupt)
+};
+class queen {
+	public:
+//	void initiate (button interrupt)
+};
+class bishop {
+	public:
+//	void initiate (button interrupt)
+};
+class knight {
+	public:
+//	void initiate (button interrupt)
+};
+class rook {
+	public:
+//	void initiate (button interrupt)
+};
+class pawn {
+	public:
+//	void initiate (button interrupt)
+};
+const char topking = 'a';
+const char topqueen = 'b';
+const char toprook = 'c';
+const char topbishop = 'd';
+const char topknight = 'e';
+const char toppawn = 'f';
+const char btmking = 'u';
+const char btmqueen = 'v';
+const char btmrook = 'w';
+const char btmbishop = 'x';
+const char btmknight = 'y';
+const char btmpawn = 'z';
+
+int main() {
+	int turnNo = 0;
+	int boardcol = 8;
+	int boardrow = 8;
+	char board [boardcol][boardrow] = {{'c','e','d','a','b','d','e','c'},	{'f','f','f','f','f','f','f','f'},{'.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.'},{'z','z','z','z','z','z','z','z'},{'w','y','x','u','v','x','y','w'}};
+
+
+
+return 0;}
