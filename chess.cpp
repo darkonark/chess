a7409f5e4200a023e03035e4d37c60bdd727045d
darkonark <darkonark@yahoo.com>
2017-02-28 15:17:36 -0500

    chess.cpp
    added a function to print the board based on a for loop

--- chess.cpp
+++ chess.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include<string>
 using namespace std;
+
+char board [8][8] = {{'R','N','B','K','Q','B','N','N'},	{'P','P','P','P','P','P','P','P'},{'.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.'},{'p','p','p','p','p','p','p','p'},{'r','n','b','k','q','b','n','r'}};
+
+
+
+
 class piece {
 	public:
 	//stuff
@@ -28,25 +35,35 @@ class pawn {
 	public:
 //	void initiate (button interrupt)
 };
-const char topking = 'a';
-const char topqueen = 'b';
-const char toprook = 'c';
-const char topbishop = 'd';
-const char topknight = 'e';
-const char toppawn = 'f';
-const char btmking = 'u';
-const char btmqueen = 'v';
-const char btmrook = 'w';
-const char btmbishop = 'x';
-const char btmknight = 'y';
-const char btmpawn = 'z';
+const char topking = 'K';
+const char topqueen = 'Q';
+const char toprook = 'R';
+const char topbishop = 'B';
+const char topknight = 'N';
+const char toppawn = 'P';
+const char btmking = 'k';
+const char btmqueen = 'q';
+const char btmrook = 'r';
+const char btmbishop = 'b';
+const char btmknight = 'n';
+const char btmpawn = 'p';
+const char empty ='.';
+const char canmove ='X';
+
+void printboard (void){
+	int col, row;
+	
+	for (row = 0; row <8; row++){
+		
+		for (col =0; col <8; col++){
+			cout<<" "<<board[row][col];
+		}
+		cout<<endl;}}
+//end of printboard function
 
 int main() {
 	int turnNo = 0;
-	int boardcol = 8;
-	int boardrow = 8;
-	char board [boardcol][boardrow] = {{'c','e','d','a','b','d','e','c'},	{'f','f','f','f','f','f','f','f'},{'.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.'},{'.','.','.','.','.','.','.','.'},{'z','z','z','z','z','z','z','z'},{'w','y','x','u','v','x','y','w'}};
 
-
+printboard();
 
 return 0;}
